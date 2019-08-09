5ee6860113e0bb3e57dcd099ca38ed176f7595f5
Adrian Benko <benko.adrian@gmail.com>
2019-08-09 21:43:41 +0200

    Update trelloData.js

--- data/trelloData.js
+++ data/trelloData.js
@@ -3,8 +3,8 @@ module.exports = {
 		BASIC_API_URL: "https://api.trello.com/1/",
 
 		LOGIN: {
-			API_KEY: "5c05b1b8103b1137da3e2c5e96d9a3d5",
-			TOKEN: "b5535ddebe9575c55f0a038882554105c4eb7e818f9884aa5dadb234a73b99da"			
+			API_KEY: "<YOUR_KEY>",
+			TOKEN: "<YOUR_token>"			
 		},
 
 		BOARD: {

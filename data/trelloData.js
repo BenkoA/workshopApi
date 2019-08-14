02790eec2dea50219121ceb71a5f39ea3a18b907
Adrian Benko <benko.adrian@gmail.com>
2019-08-14 19:52:11 +0200

    Updating and adding more test data for the Workshop

--- data/trelloData.js
+++ data/trelloData.js
@@ -1,20 +1,30 @@
 module.exports = {
-	
-		BASIC_API_URL: "https://api.trello.com/1/",
 
-		LOGIN: {
-			API_KEY: "<YOUR_KEY>",
-			TOKEN: "<YOUR_token>"			
-		},
+    BASIC_API_URL: "https://api.trello.com/1/",
 
-		BOARD: {
-			BOARDS_URL: "boards/",
-			BOARD_ID: "5anRfsNa"
-		},
+    LOGIN: {
+        API_KEY: "<YourApiKey>",
+        TOKEN: "<YourToken>"
+    },
 
-		LIST:{
-			LIST_NAMES: ["To Do", "In Progress", "Code Review", "Testing", "Done"]
-		}
-	
-	
+    BOARD: {
+        BOARDS_URL: "boards/",
+        BOARD_ID: "5anRfsNa"
+    },
+
+    LIST: {
+        LISTS_URL: "lists",
+        LIST_NAMES: ["To Do", "In Progress", "Code Review", "Testing", "Done"],
+        TO_DO_LIST_ID: "5d45479526d70c8aecaabbd4",
+    },
+
+    CARD: {
+        CARD_PROPERTIES: ["id", "checkItemStates", "closed", "dateLastActivity", "desc", "descData", "dueReminder",
+            "idBoard", "idList", "idMembersVoted", "idShort", "idAttachmentCover", "idLabels", "manualCoverAttachment",
+            "name", "pos", "shortLink", "badges", "dueComplete", "due", "idChecklists", "idMembers", "labels",
+            "shortUrl", "subscribed", "url"],
+        CARD_BADGES_PROPERTIES: ["attachmentsByType", "location", "votes", "viewingMemberVoted", "subscribed",
+            "fogbugz", "checkItems", "checkItemsChecked", "comments", "attachments", "description", "due",
+            "dueComplete"]
+    }
 };

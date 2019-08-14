ccb8459353fc861998a09774dd1ef24c2b683ffe
Adrian Benko <benko.adrian@gmail.com>
2019-08-14 19:53:35 +0200

    Adding tests for trello lists

--- tests/trelloListTest.js
+++ tests/trelloListTest.js
@@ -1,12 +1,12 @@
 const HttpStatus = require('http-status-codes');
 const expect = require('chai').expect;
 let trelloListsUrl;
-let getListResponse;
+let getListsResponse;
 
 Feature('Trello List Test - ', {retries: process.env.retries});
 
 BeforeSuite(async function (I, trelloData) {
-    trelloListsUrl = trelloData.BASIC_API_URL + trelloData.BOARD.BOARDS_URL + trelloData.BOARD.BOARD_ID + '/?fields=name&lists=all&list_fields_all&key='+trelloData.LOGIN.API_KEY+'&token=' + trelloData.LOGIN.TOKEN;
+    trelloListsUrl = trelloData.BASIC_API_URL + trelloData.BOARD.BOARDS_URL + trelloData.BOARD.BOARD_ID + '/?fields=name&lists=all&list_fields_all&key=' + trelloData.LOGIN.API_KEY + '&token=' + trelloData.LOGIN.TOKEN;
 });
 
 Before(async function (I) {
@@ -14,49 +14,61 @@ Before(async function (I) {
     expect(getListsResponse.status).to.equal(HttpStatus.OK);
 });
 
-Scenario('test lists - it should return all attributes', function (I) {
-  expect(getListsResponse.data).to.have.property('id');
-  expect(getListsResponse.data).to.have.property('name');
-  expect(getListsResponse.data).to.have.property('lists');
+Scenario('test lists - it should return all attributes', function () {
+    expect(getListsResponse.data).to.have.property('id');
+    expect(getListsResponse.data).to.have.property('name');
+    expect(getListsResponse.data).to.have.property('lists');
 }).tag('@lists');
 
-Scenario('test lists - it should return the correct type', function (I) {
-  expect(getListsResponse.data.id).to.be.a('string');
-  expect(getListsResponse.data.name).to.be.a('string');
-  expect(getListsResponse.data.lists).to.be.a('array');
+Scenario('test lists - it should return the correct type', function () {
+    expect(getListsResponse.data.id).to.be.a('string');
+    expect(getListsResponse.data.name).to.be.a('string');
+    expect(getListsResponse.data.lists).to.be.a('array');
 }).tag('@lists');
 
-Scenario('test lists - it should return all lists attributes', function (I) {
-  getListsResponse.data.lists.forEach(function (currentList){
-  	expect(currentList).to.have.property('id');
-  	expect(currentList).to.have.property('name');
-  	expect(currentList).to.have.property('closed');
-  	expect(currentList).to.have.property('idBoard');
-  	expect(currentList).to.have.property('pos');
-  	expect(currentList).to.have.property('subscribed');
-  	expect(currentList).to.have.property('softLimit');
-  });
+Scenario('test lists - it should return all lists attributes', function () {
+    getListsResponse.data.lists.forEach(function (currentList) {
+        expect(currentList).to.have.property('id');
+        expect(currentList).to.have.property('name');
+        expect(currentList).to.have.property('closed');
+        expect(currentList).to.have.property('idBoard');
+        expect(currentList).to.have.property('pos');
+        expect(currentList).to.have.property('subscribed');
+        expect(currentList).to.have.property('softLimit');
+    });
 }).tag('@lists');
 
-Scenario('test lists - it should return the correct list attribute types', function (I) {
-  getListsResponse.data.lists.forEach(function (currentList){
-  	expect(currentList.id).to.be.a('string');
-  	expect(currentList.name).to.be.a('string');
-  	expect(currentList.closed).to.be.a('boolean');
-  	expect(currentList.idBoard).to.be.a('string');
-  	expect(currentList.pos).to.be.a('number');
-  	expect(currentList.subscribed).to.be.a('boolean');
-  });
+Scenario('test lists - it should return the correct list attribute types', function () {
+    getListsResponse.data.lists.forEach(function (currentList) {
+        expect(currentList.id).to.be.a('string');
+        expect(currentList.name).to.be.a('string');
+        expect(currentList.closed).to.be.a('boolean');
+        expect(currentList.idBoard).to.be.a('string');
+        expect(currentList.pos).to.be.a('number');
+        expect(currentList.subscribed).to.be.a('boolean');
+    });
 }).tag('@lists');
 
-Scenario('test lists - lists should have the same board id as the board', function (I) {
-	getListsResponse.data.lists.forEach(function (currentList) {
-			expect(currentList.idBoard).to.equal(getListsResponse.data.id);
-	});
+Scenario('test lists - lists should have the same board id as the board', function () {
+    getListsResponse.data.lists.forEach(function (currentList) {
+        expect(currentList.idBoard).to.equal(getListsResponse.data.id);
+    });
 }).tag('@lists');
 
-Scenario('test lists - lists should have the correct names', function (I, trelloData) {
-	getListsResponse.data.lists.forEach(function (currentList) {
-			expect(trelloData.LIST.LIST_NAMES).to.include(currentList.name);
-	});
+Scenario('test lists - lists should have the correct names', function (trelloData) {
+    getListsResponse.data.lists.forEach(function (currentList) {
+        expect(trelloData.LIST.LIST_NAMES).to.include(currentList.name);
+    });
+}).tag('@lists');
+
+Scenario('test lists - lists should not be closed', function () {
+    getListsResponse.data.lists.forEach(function (currentList) {
+        expect(currentList.closed).to.equal(false);
+    });
+}).tag('@lists');
+
+Scenario('test lists - softLimits should not be set', function () {
+    getListsResponse.data.lists.forEach(function (currentList) {
+        expect(currentList.softLimit).to.be.null;
+    });
 }).tag('@lists');

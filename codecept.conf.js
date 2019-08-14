5375bade23932a8cb7360d85a83227d7294aa6f6
Adrian Benko <benko.adrian@gmail.com>
2019-08-14 19:54:15 +0200

    Updating and Refactoring Config

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,50 +1,39 @@
 exports.config = {
-  tests: './tests/*Test.js',
-  timeout: 60000,
-  output: './output',
-  helpers: {
-    REST: {
+    tests: './tests/*Test.js',
+    timeout: 60000,
+    output: './output',
+    helpers: {
+        REST: {
             defaultHeaders: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
             },
-        },
-    WebDriver: {
-      url: 'http://localhost',
-      browser: 'chrome'
-    }
-  },
-  include: {
-    // data
-    trelloData: './data/trelloData.js'
+        }
+    },
+    include: {
+        // data
+        trelloData: './data/trelloData.js'
 
-  },
-  bootstrap: null,
-  mocha: {
-    reporterOptions: {
-        reportDir: './output',
-        enableCharts: true,
-        reportTitle: 'Workshop API Test Reporter',
-        mochaFile: "output/result.xml",
-        rootSuiteTitle: ' API Tests'
-    }    
-  },
-  plugins: {
-    "allure": {
-      outputDir: './output',
-      enableScreenshotDiffPlugin: false
-    }
-  },
-  name: 'workshopApi'
-}
-
-exports.config.helpers.WebDriver.desiredCapabilities = {
-        browserName: 'chrome',
-        chromeOptions: {
-            args: [
-                "--headless",
-                "--disable-gpu",
-                "--lang=en"
-            ]
+    },
+    bootstrap: null,
+    mocha: {
+        reporterOptions: {
+            reportDir: './output',
+            enableCharts: true,
+            reportTitle: 'Workshop API Test Reporter',
+            mochaFile: "output/result.xml",
+            rootSuiteTitle: 'API Tests'
+        }
+    },
+    plugins: {
+        "allure": {
+            outputDir: './output',
+            enableScreenshotDiffPlugin: false
         },
-    };
+        wdio: {
+            enabled: true,
+            services: ['selenium-standalone']
+        }
+    },
+    name: 'workshopApi'
+};
